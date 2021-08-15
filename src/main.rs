028d1b6f29a784a9ea6c501cd42d65772eb46dbc
Mendelt Siebenga <mendelt@mendelt.nl>
2021-08-15 23:03:31 +0200

    use rawsock to capture packets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rawsock::open_best_library;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Opening packet capturing library");
+    let lib = open_best_library().expect("Could not open any packet capturing library");
+    println!("Library opened, version is {}", lib.version());
+    let interf_name = lib.all_interfaces()
+        .expect("Could not obtain interface list").first()
+        .expect("There are no available interfaces").name.clone();
+    println!("Opening the {} interface", &interf_name);
+    let mut interf = lib.open_interface(&interf_name).expect("Could not open network interface");
+    println!("Interface opened, data link: {}", interf.data_link());
+
+    //receive some packets.
+    println!("Receiving 5 packets:");
+    for _ in 0..5 {
+        let packet = interf.receive().expect("Could not receive packet");
+        println!("Received packet: {}", packet);
+    }
 }
